9e9d0faf087b45c3f97437d5dcd8b072a1265ac1
trishandulaksha <trishandulaksha@gmail.com>
2024-04-28 03:20:47 +0530

    login function updated

--- src/pages/loginPage/loginPage.tsx
+++ src/pages/loginPage/loginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import InputFieldUnit from "../../Component/InputFieldComponent/InputFieldUnit";
 import { useNavigate } from "react-router-dom";
 
@@ -26,77 +26,87 @@ function LoginPage() {
         user: { type },
       } = data;
       if (token && type === "ADMIN" && status === "ACTIVE") {
-        navigate("adminpanel");
+        setTimeout(() => {
+          navigate("adminpanel");
+          setSuccess(true);
+        }, 2000);
       }
     }
   };
 
+  useEffect(() => {
+    handleUserNavigation();
+  }, [dbResponse]);
+
   return (
-    <div className="flex items-center justify-center h-screen ">
-      <div className="border border-blue-600 w-3/4  sm:w-2/5 border-t-[20px] p-4 lg:p-14 md:p-6 sm:p-5 shadow-lg">
-        {/* Header tags */}
+    <>
+      <div className="mt-3 mx-52">
         {success && <Notification message="User Login successfully!" />}
-        <div className="w-full">
-          <div className="mb-8 font-sans text-center text-gray-600 ">
-            <h1 className="text-3xl font-bold ">Login</h1>
+      </div>
+      <div className="flex items-center justify-center h-screen ">
+        <div className="border border-blue-600 w-3/4  sm:w-2/5 border-t-[20px] p-4 lg:p-14 md:p-6 sm:p-5 shadow-lg">
+          {/* Header tags */}
+          <div className="w-full">
+            <div className="mb-8 font-sans text-center text-gray-600 ">
+              <h1 className="text-3xl font-bold ">Login</h1>
 
-            <p className="mt-1 font-sans ">
-              Lorem ipsum dolor sit amet consectetur.Risus commodo faucibus
-              pellentesque habitan.Tincidunt.
-            </p>
-          </div>
-
-          {/* Form componenet */}
-          <div className="pb-6 ">
-            <div>
-              {dbResponse && dbResponse.data && dbResponse.data.error ? (
-                <p className="text-red-700">
-                  {dbResponse.data.error || dbResponse.error}
-                </p>
-              ) : null}
+              <p className="mt-1 font-sans ">
+                Lorem ipsum dolor sit amet consectetur.Risus commodo faucibus
+                pellentesque habitan.Tincidunt.
+              </p>
             </div>
-            <div className="">
-              <form
-                action=""
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  loginDataHandler(e, setDbResponse, setSuccess);
-                  handleUserNavigation();
-                }}
-              >
-                <InputFieldUnit
-                  setCanSubmit={setCanSubmit}
-                  type="text"
-                  placeholder="Email"
-                  errMsgBase="email"
-                  name="email"
-                />
-                <InputFieldUnit
-                  setCanSubmit={setCanSubmit}
-                  type="password"
-                  placeholder="Password"
-                  errMsgBase="password"
-                  name="password"
-                />
-                <p className="text-right">Forget Password</p>
 
-                <div className="mt-7 ">
-                  <div className="h-8">
-                    <button
-                      type="submit"
-                      className="w-full py-2 text-white transition duration-300 ease-in-out bg-blue-600 rounded-lg cursor-pointer hover:bg-blue-400 hover:mt-1 "
-                      disabled={!canSubmit}
-                    >
-                      Submit
-                    </button>
+            {/* Form componenet */}
+            <div className="pb-6 ">
+              <div>
+                {dbResponse && dbResponse.data && dbResponse.data.error ? (
+                  <p className="text-red-700">
+                    {dbResponse.data.error || dbResponse.error}
+                  </p>
+                ) : null}
+              </div>
+              <div className="">
+                <form
+                  action=""
+                  onSubmit={(e) => {
+                    loginDataHandler(e, setDbResponse, setSuccess);
+                    handleUserNavigation();
+                  }}
+                >
+                  <InputFieldUnit
+                    setCanSubmit={setCanSubmit}
+                    type="text"
+                    placeholder="Email"
+                    errMsgBase="email"
+                    name="email"
+                  />
+                  <InputFieldUnit
+                    setCanSubmit={setCanSubmit}
+                    type="password"
+                    placeholder="Password"
+                    errMsgBase="password"
+                    name="password"
+                  />
+                  <p className="text-right">Forget Password</p>
+
+                  <div className="mt-7 ">
+                    <div className="h-8">
+                      <button
+                        type="submit"
+                        className="w-full py-2 text-white transition duration-300 ease-in-out bg-blue-600 rounded-lg cursor-pointer hover:bg-blue-400 hover:mt-1 "
+                        disabled={!canSubmit}
+                      >
+                        Submit
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
