83695da47c220fc19f5dc7ef13954d502f0058da
trishandulaksha <trishandulaksha@gmail.com>
2024-04-27 13:58:38 +0530

    admin panel styled

--- src/pages/adminPage/adminPanel/userListPage.tsx
+++ src/pages/adminPage/adminPanel/userListPage.tsx
@@ -3,39 +3,42 @@ import React from "react";
 function UserListPage() {
   return (
     <>
-      <div>
-        <div>
-          <h1>User List</h1>
-          <p>Lorem ipsum dolor sit amet consectetur </p>
-        </div>
-        {/* User details table */}
-        <div>
-          <table className="text-center ">
-            <tr className="p-4 bg-slate-300">
-              <th className="p-4 mx-4">First Name</th>
-              <th>Last Name</th>
-              <th>Date of Birth</th>
-              <th>Gender</th>
-              <th>Contact Number</th>
-              <th>Email</th>
-            </tr>
-            <tr className="border-b-2">
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-            </tr>
-            <tr className="border-b-2">
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-              <td>Sandaru</td>
-            </tr>
-          </table>
+      <div className="flex h-screen ">
+        <div className="fixed top-0 left-0 w-full h-full "></div>
+        <div className="relative z-10 w-full m-8 bg-white shadow-2xl shadow-gray-500">
+          <div className="items-center justify-center mt-6 mx-11">
+            <h1 className="text-2xl font-bold text-gray-800">User List</h1>
+            <p>Lorem ipsum dolor sit amet consectetur </p>
+            {/* User details table */}
+            <div className="mt-8">
+              <table className="w-full text-center ">
+                <tr className="p-4 bg-slate-300">
+                  <th className="p-4 mx-4">First Name</th>
+                  <th>Last Name</th>
+                  <th>Date of Birth</th>
+                  <th>Gender</th>
+                  <th>Contact Number</th>
+                  <th>Email</th>
+                </tr>
+                <tr className="border-b-2 ">
+                  <td className="p-4 mx-4">Sandaru</td>
+                  <td>Sandaru</td>
+                  <td>Sandaru</td>
+                  <td>Sandaru</td>
+                  <td>Sandaru</td>
+                  <td>Sandaru</td>
+                </tr>
+                <tr className="border-b-2">
+                  <td className="p-4 mx-4">Sandaru</td>
+                  <td>Sandaru</td>
+                  <td>Sandaru</td>
+                  <td>Sandaru</td>
+                  <td>Sandaru</td>
+                  <td>Sandaru</td>
+                </tr>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- src/pages/adminPage/adminPanel/userOnboardingPage.tsx
+++ src/pages/adminPage/adminPanel/userOnboardingPage.tsx
@@ -4,95 +4,100 @@ import InputFieldUnit from "../../../Component/InputFieldComponent/InputFieldUni
 function UserOnboardingPage() {
   const [canSubmit, setCanSubmit] = useState<boolean>(false);
   return (
-    <div>
-      <div>
-        <div>
-          <h1>User Onboarding</h1>
-          <p>Lorem ipsum dolor sit amet consectetur </p>
-        </div>
-        <form action="">
-          <div className="border border-black">
-            <div>
-              <p>Basic Details</p>
-              <div className="grid grid-cols-3">
-                <div>
-                  <InputFieldUnit
-                    setCanSubmit={setCanSubmit}
-                    type="text"
-                    placeholder="First Name"
-                    errMsgBase="firstname"
-                  />
-                </div>
+    <div className="flex h-screen ">
+      <div className="fixed top-0 left-0 w-full h-full "></div>
+      <div className="relative z-10 w-full m-8 bg-white shadow-2xl shadow-gray-500">
+        <div className="items-center justify-center my-3 mx-11">
+          <div>
+            <h1 className="text-2xl font-bold text-gray-800">
+              User Onboarding
+            </h1>
+            <p>Lorem ipsum dolor sit amet consectetur </p>
+          </div>
+          <form action="">
+            <div className="p-4 mt-4 border border-gray-300">
+              <div>
+                <p>Basic Details</p>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                  <div className="pl-1">
+                    <InputFieldUnit
+                      setCanSubmit={setCanSubmit}
+                      type="text"
+                      placeholder="First Name"
+                      errMsgBase="firstname"
+                    />
+                  </div>
 
-                <div>
-                  <InputFieldUnit
-                    setCanSubmit={setCanSubmit}
-                    type="text"
-                    placeholder="Last Name"
-                    errMsgBase="lastname"
-                  />
+                  <div className="pl-1">
+                    <InputFieldUnit
+                      setCanSubmit={setCanSubmit}
+                      type="text"
+                      placeholder="Last Name"
+                      errMsgBase="lastname"
+                    />
+                  </div>
+                  <div className="pl-1">
+                    <InputFieldUnit
+                      setCanSubmit={setCanSubmit}
+                      type="date"
+                      placeholder=""
+                      errMsgBase="DOB"
+                    />
+                  </div>
+                  <div className="pl-1 mt-2">
+                    <input
+                      list="GenderOption"
+                      name="Gender"
+                      placeholder="Gender"
+                      className="w-full px-4 py-2 leading-tight text-gray-700 transition duration-300 ease-in-out border-2 border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-blue-400"
+                    />
+                    <datalist id="GenderOption">
+                      <option value="Male" />
+                      <option value="Female" />
+                    </datalist>
+                  </div>
                 </div>
-                <div>
-                  <InputFieldUnit
-                    setCanSubmit={setCanSubmit}
-                    type="date"
-                    placeholder=""
-                    errMsgBase="DOB"
-                  />
-                </div>
-                <div>
-                  <input
-                    list="GenderOption"
-                    name="Gender"
-                    placeholder="Gender"
-                    className="w-full px-4 py-2 leading-tight text-gray-700 transition duration-300 ease-in-out border-2 border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-blue-400"
-                  />
-                  <datalist id="GenderOption">
-                    <option value="Male" />
-                    <option value="Female" />
-                  </datalist>
+              </div>
+              <div className="mt-6">
+                <p>Contact Details</p>
+                <div className="grid grid-cols-3">
+                  <div className="pl-1">
+                    <InputFieldUnit
+                      setCanSubmit={setCanSubmit}
+                      type="text"
+                      placeholder="Mobile Number"
+                      errMsgBase="mobilenumber"
+                    />
+                  </div>
+                  <div className="pl-1">
+                    <InputFieldUnit
+                      setCanSubmit={setCanSubmit}
+                      type="text"
+                      placeholder="Email"
+                      errMsgBase="email"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-            <div>
-              <p>Basic Details</p>
-              <div className="grid grid-cols-3">
-                <div>
-                  <InputFieldUnit
-                    setCanSubmit={setCanSubmit}
-                    type="text"
-                    placeholder="Mobile Number"
-                    errMsgBase="mobilenumber"
-                  />
-                </div>
-                <div>
-                  <InputFieldUnit
-                    setCanSubmit={setCanSubmit}
-                    type="text"
-                    placeholder="Email"
-                    errMsgBase="email"
-                  />
-                </div>
+            <div className="flex justify-end mt-8">
+              <div>
+                <input
+                  type="button"
+                  value="Clear"
+                  className="py-2 text-blue-700 border border-blue-700 shadow-xl cursor-pointer px-7 hover:rounded-md"
+                />
+              </div>
+              <div className="ml-3">
+                <input
+                  type="button"
+                  value="Save"
+                  className="py-2 text-white bg-blue-700 border border-blue-700 shadow-xl cursor-pointer px-7 hover:rounded-md"
+                />
               </div>
             </div>
-          </div>
-          <div className="flex justify-end mt-8">
-            <div>
-              <input
-                type="button"
-                value="Clear"
-                className="text-blue-700 border border-blue-700"
-              />
-            </div>
-            <div>
-              <input
-                type="button"
-                value="Save"
-                className="text-white bg-blue-700 border border-blue-700"
-              />
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
